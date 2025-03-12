4c49baab041a6073952782868423b583cc7eee8c
cmshanker <cassandrashanker@gmail.com>
2025-03-12 17:26:48 -0400

    building out initial components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
+import Box from '@mui/material/Box'
+import Divider from '@mui/material/Divider'
+import Grid from '@mui/material/Grid2'
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
 
-import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Graph from './components/Graph'
 
 const App = (): React.ReactElement => {
-  const [count, setCount] = useState(0)
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Box sx={{ display: 'flex' }}>
+      <Box sx={{ width: '100%', maxWidth: { sm: '100%', md: '1700px' } }}>
+        <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
+          Overview
+        </Typography>
+        <Grid container spacing={4}>
+          <Grid size={6}>
+            <Graph />
+          </Grid>
+          <Grid size={6}>
+            <Stack
+              spacing={4}
+              divider={<Divider orientation="horizontal" flexItem />}
+            >
+              <Box sx={{ width: '100%' }}></Box>
+              <Box sx={{ width: '100%' }}></Box>
+            </Stack>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
   )
 }
 
